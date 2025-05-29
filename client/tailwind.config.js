77a3b6762e30c6b7fe7dd43774de13fbae797b68
saleh <ghalayinisaleh9@gmail.com>
2025-05-29 17:05:41 +0300

    chore: initialized the configurations with vite, Tailwind CSS, and other basic config files

---
+++ client/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+  extend: {
+    colors: {
+      primary: '#F97316',
+      background: '#FFFBF7',
+      textPrimary: '#1F2937',
+      muted: '#9CA3AF',
+      error: '#DC2626',
+      success: '#22C55E',
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+  },
+},
+  plugins: [],
+}
