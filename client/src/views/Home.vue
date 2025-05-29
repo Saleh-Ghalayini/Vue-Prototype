a403b40e441503e9b4f95e2478805206bf06e5e2
saleh <ghalayinisaleh9@gmail.com>
2025-05-29 17:22:46 +0300

    feat(login): initiated Home view (Recruiter Portal) with basic layout.

---
+++ client/src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-[#FFFBF7]">
+    <div class="p-8 bg-white rounded shadow text-[#1F2937]">
+      Home Page (Recruiter Portal)
+    </div>
+  </div>
+</template>
