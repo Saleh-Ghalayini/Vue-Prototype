cc00b625e3c62dce7ac7bfbd224febcb7f89ea73
saleh <ghalayinisaleh9@gmail.com>
2025-05-30 00:45:37 +0300

    feat(main): mounted the toast plugin into the main.

--- client/src/main.js
+++ client/src/main.js
@@ -3,8 +3,10 @@ import router from './router'
 import './assets/tailwind.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ToastPlugin from './plugins/toast'
 
 const app = createApp(App)
 app.use(router)
 app.use(createPinia())
+app.use(ToastPlugin)
 app.mount('#app')
