307ffe69944af1135291c0dd0c062998d8171ee9
saleh <ghalayinisaleh9@gmail.com>
2025-05-30 01:24:54 +0300

    core: Made Pinia available globally for API interceptors.

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,17 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import ToastPlugin from './plugins/toast'
 
 const app = createApp(App)
+const pinia = createPinia()
+
+// Making pinia available to our API interceptors
+window.$pinia = pinia
+
 app.use(router)
-app.use(createPinia())
+app.use(pinia)
 app.use(ToastPlugin)
 app.mount('#app')
