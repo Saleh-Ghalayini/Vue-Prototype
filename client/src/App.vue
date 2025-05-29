799dbc2b0a180e792407e0d0354461cf811806f1
saleh <ghalayinisaleh9@gmail.com>
2025-05-30 01:25:18 +0300

    feat(auth): Added automatic authentication state restoration.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,14 @@
 <template>
   <router-view />
 </template>
+
+<script setup>
+import { onMounted } from 'vue';
+import { useAuth } from './composables/useAuth';
+
+const { checkAuth } = useAuth();
+
+onMounted(() => {
+  checkAuth();
+});
+</script>
