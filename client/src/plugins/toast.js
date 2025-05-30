ea133f49155868720591eeeb740d908a82378355
saleh <ghalayinisaleh9@gmail.com>
2025-05-30 06:12:29 +0300

    refactor(toast-plugin): added some toast logic.

--- client/src/plugins/toast.js
+++ client/src/plugins/toast.js
@@ -1,22 +1,18 @@
 import { createApp } from 'vue';
-import ToastContainer from '../components/ToastContainer.vue';
 import toastService from '../services/toastService';
+import ToastContainer from '../components/ToastContainer.vue';
 
 const ToastPlugin = {
   install: (app) => {
-    // Make toastService available globally
     app.config.globalProperties.$toast = toastService;
     
-    // Create a div for mounting the toast container
     const toastContainerElement = document.createElement('div');
     toastContainerElement.id = 'toast-container';
     document.body.appendChild(toastContainerElement);
     
-    // Create and mount the ToastContainer component
     const ToastContainerApp = createApp(ToastContainer);
     ToastContainerApp.mount(toastContainerElement);
     
-    // Provide the toast service to all components
     app.provide('toast', toastService);
   }
 };
