e50d59606ebe7d0b106f1a7b4bf422601d1dd5fa
saleh <ghalayinisaleh9@gmail.com>
2025-05-30 03:40:32 +0300

    feat(router): added the register route.

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,9 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '../store/auth'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
 
 const routes = [
   { path: '/', name: 'Login', component: Login },
+  { path: '/register', name: 'Register', component: Register },
   { path: '/home', name: 'Home', component: Home, meta: { requiresAuth: true } },
 ]
 
